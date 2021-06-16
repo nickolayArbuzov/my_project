145ab7f54bb20b250b596b2f773c81ef641cdc1e
nickolayArbuzov <nickarbuzov@yandex.by>
2021-06-17 00:02:50 +0300

    fix: deleted doubled files

--- src/components/login/api.tsx
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-
-export const login =(email: string, password: string, rememberMe: boolean) => {
-    return axios.post('http://localhost:7542/2.0/auth/login', {email, password, rememberMe}, {
-        withCredentials: true,
-    })
-}
--- src/state/reducers/enteringNewPassword.ts
+++
@@ -1,8 +0,0 @@
-const initialState = {}
-
-type initialStateType = typeof initialState
-
-export const enteringNewPasswordReducer = (state: initialStateType = initialState, action: actionType) => {
-    return state
-}
-type actionType = {}
